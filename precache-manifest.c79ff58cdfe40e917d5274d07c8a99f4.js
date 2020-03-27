57b0f65018640612e8b905bb6471302daf960a90
zee-bit <equbalzeeshan@gmail.com>
2020-03-27 21:07:13 +0530

    Updates

---
+++ precache-manifest.c79ff58cdfe40e917d5274d07c8a99f4.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cc17625af3e9311839df324f1dc87c4f",
+    "url": "/MyFirstPortfolio/index.html"
+  },
+  {
+    "revision": "ad5c314d46f1e6e4506a",
+    "url": "/MyFirstPortfolio/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "e52534184811843e79f5",
+    "url": "/MyFirstPortfolio/static/js/2.4f3f866b.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/MyFirstPortfolio/static/js/2.4f3f866b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ad5c314d46f1e6e4506a",
+    "url": "/MyFirstPortfolio/static/js/main.46e4f43d.chunk.js"
+  },
+  {
+    "revision": "01067c6bccb269c38b04",
+    "url": "/MyFirstPortfolio/static/js/runtime-main.c875b6f7.js"
+  },
+  {
+    "revision": "05d8a11a51b95e9b7c030849f4fa5803",
+    "url": "/MyFirstPortfolio/static/media/profilePic.05d8a11a.jpg"
+  }
+]);
